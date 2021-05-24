071d13a4fc6ca64dbe42011fb21a9ca5fad312c0
Saad Alghadhban <com-com141@hotmail.com>
2021-05-24 22:28:49 +0300

    updating

--- src/context.js
+++ src/context.js
@@ -91,55 +91,118 @@ export const AppProvider = ({children}) =>{
     const fetchData = async (url)=>{
         const response = await fetch(url)
         if (response.ok){
-        const info = await response.json();
-        let today =  new Date();
-        let hour = today.getHours() -1;
-        let cityName = info.location.name;
-        let countryHour = parseInt(info.location.localtime.slice(11,13))
-        console.log(countryHour);
-        let countryName = info.location.country;
-        let currentTemp = Math.round(info.current.temp_c)
-        let weatherDescription = info.current.condition.text
-        let tempA = Math.round(info.forecast.forecastday[0].hour[countryHour +1>= 24?countryHour +1 -24:countryHour +1].temp_c)
-        let tempB = Math.round(info.forecast.forecastday[0].hour[countryHour +2>= 24?countryHour +2 -24:countryHour +2].temp_c)
-        let tempC = Math.round(info.forecast.forecastday[0].hour[countryHour +3>= 24?countryHour +3 -24:countryHour +3].temp_c)
-        let tempD = Math.round(info.forecast.forecastday[0].hour[countryHour +4>= 24?countryHour +4 -24:countryHour +4].temp_c)
-        let id = uuidv4()
-        let condA = info.forecast.forecastday[0].hour[countryHour +0>= 24?countryHour +0 -24:countryHour +0].condition.text
-        let condB = info.forecast.forecastday[0].hour[countryHour +1>= 24?countryHour +1 -24:countryHour +1].condition.text
-        let condC = info.forecast.forecastday[0].hour[countryHour +2>= 24?countryHour +2 -24:countryHour +2].condition.text
-        let condD = info.forecast.forecastday[0].hour[countryHour +3>= 24?countryHour +3 -24:countryHour +3].condition.text
-        // countryHour+1 ||countryHour+2||countryHour+3||countryHour+4 >= 24?1:0
-        // let newDayA = countryHour +1>= 24?countryHour +1 -24:countryHour +1
-        // let newDayB = countryHour +2>= 24?countryHour +2 -24:countryHour +2
-        // let newDayC = countryHour +3>= 24?countryHour +3 -24:countryHour +3
-        // let newDayD = countryHour +4>= 24?countryHour +4 -24:countryHour +4
-        // let weatherAA = Math.round(info.forecast)
-        let isDay = info.current.is_day;
-        console.log(isDay);
-        console.log("THISSSSSS"+condB);
-        let isDayA = info.forecast.forecastday[0].hour[countryHour +0>= 24?countryHour +0 -24:countryHour +0].is_day
-        console.log(isDayA);
-        let isDayB = info.forecast.forecastday[0].hour[countryHour +1>= 24?countryHour +1 -24:countryHour +1].is_day
-        let isDayC = info.forecast.forecastday[0].hour[countryHour +2>= 24?countryHour +2 -24:countryHour +2].is_day
-        let isDayD = info.forecast.forecastday[0].hour[countryHour +3>= 24?countryHour +3 -24:countryHour +3].is_day
-        let maxTemp = Math.round(info.forecast.forecastday[0].day.maxtemp_c)
-        let minTemp =Math.round(info.forecast.forecastday[0].day.mintemp_c)
-        let windSpeed = info.forecast.forecastday[0].day.maxwind_kph
-        let uv = info.forecast.forecastday[0].day.uv
-        let sunRise = info.forecast.forecastday[0].astro.sunrise
-        let sunSet = info.forecast.forecastday[0].astro.sunset
-        let currentDay = info.current.last_updated.slice(0,10)
-        const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        let d = new Date(currentDay);
-        let dayName = weekDays[d.getDay()];
-        
-         
-        
-        
-        setState([...state,{id,dayName,maxTemp,minTemp,sunRise,sunSet,windSpeed,uv,cityName,countryName,countryHour,currentTemp,weatherDescription,isDay,nextWeather: {weatherA:[isDayA,condA, tempA],weatherB:[isDayB,condB,tempB],weatherC:[isDayC,condC,tempC],weatherD:[isDayD,condD,tempD]}}])
-        console.log(info);
+          const info = await response.json();
+          let today = new Date();
+          let hour = today.getHours() - 1;
+          let cityName = info.location.name;
+          let countryHour = parseInt(info.location.localtime.slice(11, 13));
+          console.log(countryHour);
+          let countryName = info.location.country;
+          let currentTemp = Math.round(info.current.temp_c);
+          let weatherDescription = info.current.condition.text;
+          let tempA = Math.round(
+            info.forecast.forecastday[0].hour[
+              countryHour + 1 >= 24 ? countryHour + 1 - 24 : countryHour + 1
+            ].temp_c
+          );
+          let tempB = Math.round(
+            info.forecast.forecastday[0].hour[
+              countryHour + 2 >= 24 ? countryHour + 2 - 24 : countryHour + 2
+            ].temp_c
+          );
+          let tempC = Math.round(
+            info.forecast.forecastday[0].hour[
+              countryHour + 3 >= 24 ? countryHour + 3 - 24 : countryHour + 3
+            ].temp_c
+          );
+          let tempD = Math.round(
+            info.forecast.forecastday[0].hour[
+              countryHour + 4 >= 24 ? countryHour + 4 - 24 : countryHour + 4
+            ].temp_c
+          );
+          let id = uuidv4();
+          let condA =
+            info.forecast.forecastday[0].hour[
+              countryHour + 0 >= 24 ? countryHour + 0 - 24 : countryHour + 0
+            ].condition.text;
+          let condB =
+            info.forecast.forecastday[0].hour[
+              countryHour + 1 >= 24 ? countryHour + 1 - 24 : countryHour + 1
+            ].condition.text;
+          let condC =
+            info.forecast.forecastday[0].hour[
+              countryHour + 2 >= 24 ? countryHour + 2 - 24 : countryHour + 2
+            ].condition.text;
+          let condD =
+            info.forecast.forecastday[0].hour[
+              countryHour + 3 >= 24 ? countryHour + 3 - 24 : countryHour + 3
+            ].condition.text;
 
+          let isDay = info.current.is_day;
+          console.log(isDay);
+          console.log("THISSSSSS" + condB);
+          let isDayA =
+            info.forecast.forecastday[0].hour[
+              countryHour + 0 >= 24 ? countryHour + 0 - 24 : countryHour + 0
+            ].is_day;
+          console.log(isDayA);
+          let isDayB =
+            info.forecast.forecastday[0].hour[
+              countryHour + 1 >= 24 ? countryHour + 1 - 24 : countryHour + 1
+            ].is_day;
+          let isDayC =
+            info.forecast.forecastday[0].hour[
+              countryHour + 2 >= 24 ? countryHour + 2 - 24 : countryHour + 2
+            ].is_day;
+          let isDayD =
+            info.forecast.forecastday[0].hour[
+              countryHour + 3 >= 24 ? countryHour + 3 - 24 : countryHour + 3
+            ].is_day;
+          let maxTemp = Math.round(info.forecast.forecastday[0].day.maxtemp_c);
+          let minTemp = Math.round(info.forecast.forecastday[0].day.mintemp_c);
+          let windSpeed = info.forecast.forecastday[0].day.maxwind_kph;
+          let uv = info.forecast.forecastday[0].day.uv;
+          let sunRise = info.forecast.forecastday[0].astro.sunrise;
+          let sunSet = info.forecast.forecastday[0].astro.sunset;
+          let currentDay = info.current.last_updated.slice(0, 10);
+          const weekDays = [
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+          ];
+          let d = new Date(currentDay);
+          let dayName = weekDays[d.getDay()];
+
+          setState([
+            ...state,
+            {
+              id,
+              dayName,
+              maxTemp,
+              minTemp,
+              sunRise,
+              sunSet,
+              windSpeed,
+              uv,
+              cityName,
+              countryName,
+              countryHour,
+              currentTemp,
+              weatherDescription,
+              isDay,
+              nextWeather: {
+                weatherA: [isDayA, condA, tempA],
+                weatherB: [isDayB, condB, tempB],
+                weatherC: [isDayC, condC, tempC],
+                weatherD: [isDayD, condD, tempD],
+              },
+            },
+          ]);
+          console.log(info);
         } else {
             console.log('response error no such location');
         }
